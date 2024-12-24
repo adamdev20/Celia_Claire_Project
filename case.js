e5a41356b950e3e7d27ae1ee790c37a19303fd7e
Adam Dev <114385858+adamdev20@users.noreply.github.com>
2024-12-24 13:12:17 +0700

    Update case.js

--- case.js
+++ case.js
@@ -1,110 +1,352 @@
-require("./config")
-const fs = require('fs')
-const util = require('util')
-const axios = require('axios')
-const { exec } = require("child_process")
+require("./config");
 
-module.exports = async (Adam, m) => {
-try {
-const body = (
-(m.mtype === 'conversation' && m.message.conversation) ||
-(m.mtype === 'imageMessage' && m.message.imageMessage.caption) ||
-(m.mtype === 'documentMessage' && m.message.documentMessage.caption) ||
-(m.mtype === 'videoMessage' && m.message.videoMessage.caption) ||
-(m.mtype === 'extendedTextMessage' && m.message.extendedTextMessage.text) ||
-(m.mtype === 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ||
-(m.mtype === 'templateButtonReplyMessage' && m.message.templateButtonReplyMessage.selectedId)
-) ? (
-(m.mtype === 'conversation' && m.message.conversation) ||
-(m.mtype === 'imageMessage' && m.message.imageMessage.caption) ||
-(m.mtype === 'documentMessage' && m.message.documentMessage.caption) ||
-(m.mtype === 'videoMessage' && m.message.videoMessage.caption) ||
-(m.mtype === 'extendedTextMessage' && m.message.extendedTextMessage.text) ||
-(m.mtype === 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ||
-(m.mtype === 'templateButtonReplyMessage' && m.message.templateButtonReplyMessage.selectedId)
-) : '';
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+const { exec } = require("child_process");
+const chalk = require("chalk");
+const path = require("path");
+const moment = require("moment-timezone");
 
-const budy = (typeof m.text === 'string') ? m.text : '';
-const prefixRegex = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/;
-const prefix = prefixRegex.test(body) ? body.match(prefixRegex)[0] : '.';
-const isCmd = body.startsWith(prefix);
-const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : '';
-const args = body.trim().split(/ +/).slice(1)
-const text = q = args.join(" ")
-const sender = m.key.fromMe ? (Adam.user.id.split(':')[0]+'@s.whatsapp.net' || Adam.user.id) : (m.key.participant || m.key.remoteJid)
-const botNumber = await Adam.decodeJid(Adam.user.id)
-const senderNumber = sender.split('@')[0]
-const isCreator = (m && m.sender && [botNumber, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)) || false;
-const pushname = m.pushName || `${senderNumber}`
-const isBot = botNumber.includes(senderNumber)
+const cheerio = require("cheerio");
 
+module.exports = async (ptz, m) => {
+  try {
+    const body =
+      (m.mtype === "conversation" && m.message.conversation) ||
+      (m.mtype === "imageMessage" && m.message.imageMessage.caption) ||
+      (m.mtype === "documentMessage" && m.message.documentMessage.caption) ||
+      (m.mtype === "videoMessage" && m.message.videoMessage.caption) ||
+      (m.mtype === "extendedTextMessage" &&
+        m.message.extendedTextMessage.text) ||
+      (m.mtype === "buttonsResponseMessage" &&
+        m.message.buttonsResponseMessage.selectedButtonId) ||
+      (m.mtype === "templateButtonReplyMessage" &&
+        m.message.templateButtonReplyMessage.selectedId) ||
+      "";
 
-switch(command) {
-case "menu": case "help" :{
-m.reply(`
-halo ${pushname}, ini adalah polosan bot, jadi fiturnya sedikit.
+    const budy = typeof m.text === "string" ? m.text : "";
+    const prefixRegex = /^[°zZ#$@*+,.?=''():%^&><`~|/\\]/;
+    const prefix = prefixRegex.test(body) ? body.match(prefixRegex)[0] : ".";
+    const isCmd = body.startsWith(prefix);
+    const command = isCmd
+      ? body.slice(prefix.length).trim().split(" ").shift().toLowerCase()
+      : "";
+    const args = body.trim().split(/ +/).slice(1);
+    const text = (q = args.join(" "));
+    const sender = m.key.fromMe
+      ? ptz.user.id.split(":")[0] + "@s.whatsapp.net" || ptz.user.id
+      : m.key.participant || m.key.remoteJid;
+    const botNumber = await ptz.decodeJid(ptz.user.id);
+    const senderNumber = sender.split("@")[0];
+    const isCreator =
+      (m &&
+        m.sender &&
+        [botNumber, ...global.owner]
+          .map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net")
+          .includes(m.sender)) ||
+      false;
+    const pushname = m.pushName || `${senderNumber}`;
+    const isBot = botNumber.includes(senderNumber);
+    const color = chalk;
 
-> Menu
-~ .ai
-`)
-}
-break
-case "ai":{
-if (!text) return m.reply(`_.ai prompt_`)
-let {data} = await axios.get("https://api.mininxd.my.id/gemini/?q=" + text); 
-m.reply(data.result);
-}
-break
-default:
-if (budy.startsWith('=>')) {
-if (!isCreator) return
-function Return(sul) {
-sat = JSON.stringify(sul, null, 2)
-bang = util.format(sat)
-if (sat == undefined) {
-bang = util.format(sul)
-}
-return m.reply(bang)
-}
-try {
-m.reply(util.format(eval(`(async () => { return ${budy.slice(3)} })()`)))
-} catch (e) {
-m.reply(String(e))
-}
-}
+    let quoted = m.quoted ? m.quoted : m;
 
-if (budy.startsWith('>')) {
-if (!isCreator) return
-let kode = budy.trim().split(/ +/)[0]
-let teks
-try {
-teks = await eval(`(async () => { ${kode == ">>" ? "return" : ""} ${q}})()`)
-} catch (e) {
-teks = e
-} finally {
-await m.reply(require('util').format(teks))
-}
-}
+    if (isCmd) {
+      console.log("FROM", `${pushname}`, "Text :", `${body}`);
+    }
 
-if (budy.startsWith('$')) {
-if (!isCreator) return
-exec(budy.slice(2), (err, stdout) => {
-if (err) return m.reply(`${err}`)
-if (stdout) return m.reply(stdout)
-})
-}
-}
+    const isMedia = (message) => {
+      const mediaTypes = [
+        "imageMessage",
+        "videoMessage",
+        "audioMessage",
+        "documentMessage",
+        "stickerMessage",
+      ];
+      return mediaTypes.some((type) => message.message[type]);
+    };
 
-} catch (err) {
-console.log(util.format(err))
-}
-}
+    const isowner = (sender) => sender === global.owner;
 
+    //Registrasi
+    const registerFilePath = path.join(__dirname, "./database/register.json");
 
-let file = require.resolve(__filename)
+    const loadRegisterData = () => {
+      if (!fs.existsSync(registerFilePath)) {
+        fs.writeFileSync(registerFilePath, JSON.stringify([]));
+      }
+      return JSON.parse(fs.readFileSync(registerFilePath));
+    };
+
+    const saveRegisterData = (data) => {
+      fs.writeFileSync(registerFilePath, JSON.stringify(data, null, 2));
+    };
+
+    let registerData = loadRegisterData();
+
+    function isRegistered(sender) {
+      return registerData.some((user) => user.id === sender);
+    }
+
+    //==================[ FUNCTION WAKTU ]======================\\
+    function getFormattedDate() {
+      var currentDate = new Date();
+      var day = currentDate.getDate();
+      var month = currentDate.getMonth() + 1;
+      var year = currentDate.getFullYear();
+      var hours = currentDate.getHours();
+      var minutes = currentDate.getMinutes();
+      var seconds = currentDate.getSeconds();
+    }
+
+    let d = new Date(new Date() + 3600000);
+    let locale = "id";
+    let week = d.toLocaleDateString(locale, { weekday: "long" });
+    let date = d.toLocaleDateString(locale, {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+    const hariini = d.toLocaleDateString("id", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+    function msToTime(duration) {
+      var milliseconds = parseInt((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      return hours + " jam " + minutes + " menit " + seconds + " detik";
+    }
+
+    function msToDate(ms) {
+      temp = ms;
+      days = Math.floor(ms / (24 * 60 * 60 * 1000));
+      daysms = ms % (24 * 60 * 60 * 1000);
+      hours = Math.floor(daysms / (60 * 60 * 1000));
+      hoursms = ms % (60 * 60 * 1000);
+      minutes = Math.floor(hoursms / (60 * 1000));
+      minutesms = ms % (60 * 1000);
+      sec = Math.floor(minutesms / 1000);
+      return days + " Hari " + hours + " Jam " + minutes + " Menit";
+      // +minutes+":"+sec;
+    }
+
+    // Sayying time
+    const timee = moment().tz("Asia/Jakarta").format("HH:mm:ss");
+    if (timee < "23:59:00") {
+      var waktuucapan = "Selamat Malam";
+    }
+    if (timee < "19:00:00") {
+      var waktuucapan = "Selamat Petang";
+    }
+    if (timee < "18:00:00") {
+      var waktuucapan = "Selamat Sore";
+    }
+    if (timee < "15:00:00") {
+      var waktuucapan = "Selamat Siang";
+    }
+    if (timee < "10:00:00") {
+      var waktuucapan = "Selamat Pagi";
+    }
+    if (timee < "05:00:00") {
+      var waktuucapan = "Selamat Subuh";
+    }
+    if (timee < "03:00:00") {
+      var waktuucapan = "Tengah Malam";
+    }
+
+    switch (command) {
+      case "menu":
+      case "allmenu": {
+        if (!isRegistered(sender)) {
+          return m.reply(global.mess.register);
+        }
+
+        await ptz.sendMessage(m.chat, {
+          react: {
+            text: "🕐", // Emoji loading
+            key: m.key,
+          },
+        });
+
+        // Simulasi proses (delay)
+        setTimeout(async () => {
+          await ptz.sendMessage(m.chat, {
+            react: {
+              text: "✅", // Emoji selesai
+              key: m.key,
+            },
+          });
+        }, 200); // Ganti 3000 dengan durasi waktu yang diinginkan
+
+        const menuText = `Hallo ${pushname} ${waktuucapan}👋🏻
+    `;
+
+        const fVerif = {
+          key: {
+            participant: "0@s.whatsapp.net",
+
+            remoteJid: "status@broadcast",
+          },
+
+          message: { conversation: `_*${hariini}*_` },
+        };
+
+        await ptz.sendMessage(
+          m.chat,
+          {
+            text: menuText,
+
+            contextInfo: {
+              quotedMessage: fVerif.message,
+
+              quotedStanzaID: fVerif.key.id,
+
+              quotedParticipant: fVerif.key.participant,
+
+              externalAdReply: {
+                title: `Asisten Pribadi ${global.name}`,
+
+                body: "Powered By Adam Dev",
+
+                mediaType: 1,
+
+                thumbnail: fs.readFileSync(
+                  path.join(__dirname, "./media/menu.png")
+                ),
+
+                ai: true,
+
+                renderLargerThumbnail: false,
+
+                showAdAttribution: true,
+              },
+            },
+          },
+          { quoted: fVerif }
+        );
+
+        break;
+      }
+
+      case "register": {
+        const userExists = registerData.find((user) => user.id === sender);
+
+        if (userExists) {
+          await ptz.sendMessage(m.chat, {
+            text: "Anda sudah terdaftar sebelumnya!",
+          });
+        } else {
+          const newUser = {
+            id: sender,
+            name: pushname,
+            registeredAt: hariini,
+          };
+
+          registerData.push(newUser);
+          saveRegisterData(registerData);
+
+          await ptz.sendMessage(m.chat, {
+            text: `Registrasi berhasil! Selamat datang, ${pushname}! \n\nID: ${newUser.id}\nNama: ${newUser.name}\nWaktu Registrasi: ${hariini}`,
+          });
+        }
+        break;
+      }
+
+      case "checkuser": {
+        if (!isCreator) return m.reply(global.mess.owner);
+
+        const userList = registerData
+          .map(
+            (user) =>
+              `ID: ${user.id}\nNama: ${user.name}\nWaktu: ${user.registeredAt}`
+          )
+          .join("\n\n");
+
+        const response = userList || "Belum ada pengguna yang terdaftar.";
+
+        await ptz.sendMessage(m.chat, {
+          text: `*Pengguna Terdaftar:*\n\n${response}`,
+        });
+        break;
+      }
+
+      case "h":
+      case "hidetag":
+        if (!isRegistered(sender)) {
+          return m.reply(global.mess.register);
+        }
+        if (!isCreator) return m.reply("yang noob ga diajak😂");
+
+        if (!text) return m.reply("");
+
+        const participants = await ptz
+          .groupMetadata(m.chat)
+          .then((res) => res.participants);
+
+        await ptz.sendMessage(
+          m.chat,
+          {
+            text: text ? text : "",
+
+            mentions: participants.map((a) => a.id),
+          },
+          { quoted: m }
+        );
+
+        break;
+
+      default:
+        if (budy.startsWith("=>")) {
+          if (!isCreator) return;
+          try {
+            m.reply(
+              util.format(eval(`(async () => { return ${budy.slice(3)} })()`))
+            );
+          } catch (e) {
+            m.reply(String(e));
+          }
+        }
+
+        if (budy.startsWith(">")) {
+          if (!isCreator) return;
+          try {
+            const result = await eval(
+              `(async () => { ${
+                budy.startsWith(">>") ? "return" : ""
+              } ${q} })()`
+            );
+            m.reply(util.format(result));
+          } catch (e) {
+            m.reply(String(e));
+          }
+        }
+
+        if (budy.startsWith("$")) {
+          if (!isCreator) return;
+          exec(budy.slice(2), (err, stdout) => {
+            if (err) return m.reply(String(err));
+            if (stdout) return m.reply(stdout);
+          });
+        }
+    }
+  } catch (err) {
+    console.log(util.format(err));
+  }
+};
+
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(`Update ${__filename}`)
-delete require.cache[file]
-require(file)
-})
+  fs.unwatchFile(file);
+  console.log(`Update ${__filename}`);
+  delete require.cache[file];
+  require(file);
+});
